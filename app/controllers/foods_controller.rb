7607585fe775c691e61974600167ff2275cd3869
Mesi <mse212002@gmail.com>
2024-02-13 12:38:53 +0200

    :construction: add methods index, show, new, create and destroy to the foods_controller

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,2 +1,17 @@
 class FoodsController < ApplicationController
+  def index
+    @foods = Food.all
+  end
+
+  def show
+    @food = Food.find(params[:id])
+  end
+
+  def new
+    @new_food = Food.new
+  end
+
+  def create; end
+
+  def destroy; end
 end
