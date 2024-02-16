b6ecab5999a80d6754a9d83e8b56256c62a5d52f
Briton Otieno <ojwangbriton@gmail.com>
2024-02-16 17:34:15 +0300

    Update the recipe controller

--- app/controllers/recipe_foods_controller.rb
+++ app/controllers/recipe_foods_controller.rb
@@ -1,2 +1,29 @@
 class RecipeFoodsController < ApplicationController
+  before_action :set_recipe
+
+  def new
+    @recipe_food = @recipe.recipe_foods.build
+    @recipe_food.build_food
+  end
+
+  def create
+    @recipe_food = @recipe.recipe_foods.build(recipe_food_params)
+    if @recipe_food.save
+      respond_to do |format|
+        format.turbo_stream
+      end
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_recipe
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def recipe_food_params
+    params.require(:recipe_food).permit(:quantity, food_attributes: [:name, :quantity, :price])
+  end
 end
