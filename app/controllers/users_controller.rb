1a4eb492ba20e10e1edeb5f61d931f0036c7f977
Briton Otieno <ojwangbriton@gmail.com>
2024-02-17 20:54:23 +0300

    Add the set users method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,12 @@ class UsersController < ApplicationController
   end
 
   def show
-    @recipes = @user.recipes
+    if @user
+      @recipes = @user.recipes
+    else
+      # Handle the case where params[:id] is "sign_out"
+      redirect_to root_path, notice: 'User not found'
+    end
   end
 
   def new
@@ -36,16 +41,21 @@ class UsersController < ApplicationController
     @user.destroy
     redirect_to users_url, notice: 'User was successfully destroyed.'
   end
-  
+
   def sign_out_user
-    sign_out(current_user)
-    redirect_to root_path, notice: 'Signed out successfully'
+    if params[:id] == 'sign_out'
+      # Handle sign out without fetching a user from the database
+      redirect_to root_path, notice: 'Signed out successfully'
+    else
+      sign_out(current_user)
+      redirect_to root_path, notice: 'Signed out successfully'
+    end
   end
 
   private
 
   def set_user
-    @user = User.find(params[:id])
+    @user = params[:id] == 'sign_out' ? nil : User.find(params[:id])
   end
 
   def user_params
